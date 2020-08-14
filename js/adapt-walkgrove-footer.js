c42914216aeed1775894f639828c37971fccbfdc
@laura.lululand <laura.lululand@gmail.com>
2020-08-14 14:58:18 +0100

    update reference name

--- js/adapt-walkgrove-footer.js
+++ js/adapt-walkgrove-footer.js
@@ -49,10 +49,10 @@ define([
     }
   },
   {
-    template: 'footer'
+    template: 'pageFooter'
   });
 
-  return Adapt.register('footer', {
+  return Adapt.register('pageFooter', {
     model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
     view: PageFooterView
   });
