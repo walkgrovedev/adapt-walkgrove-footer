6f4fb2e326a334a55bfa0d94141949574531e379
@laura.lululand <laura.lululand@gmail.com>
2020-09-25 09:00:17 +0100

    Updates from Goodyear

--- js/adapt-walkgrove-footer.js
+++ js/adapt-walkgrove-footer.js
@@ -8,36 +8,15 @@ define([
 
     preRender: function() {
       this.checkIfResetOnRevisit();
+      this.setCompletionStatus();
     },
 
     postRender: function() {
       this.setReadyStatus();
-
-      this.setupInview();
+      
     },
 
-    setupInview: function() {
-      var selector = this.getInviewElementSelector();
-      if (!selector) {
-        this.setCompletionStatus();
-        return;
-      }
-
-      this.setupInviewCompletion(selector);
-    },
-
-    /**
-     * determines which element should be used for inview logic - body, instruction or title - and returns the selector for that element
-     */
-    getInviewElementSelector: function() {
-      if (this.model.get('body')) return '.component__body';
-
-      if (this.model.get('instruction')) return '.component__instruction';
-
-      if (this.model.get('displayTitle')) return '.component__title';
-
-      return null;
-    },
+    
 
     checkIfResetOnRevisit: function() {
       var isResetOnRevisit = this.model.get('_isResetOnRevisit');
